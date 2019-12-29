fda381d1856b5b639922157c2eeea823647216f4
unknown <jessica.morgan.randall@emory.edu>
2019-12-29 14:26:41 -0500

    created flu1 and flu2 datasets, updated import src code

--- import/src/import.R
+++ import/src/import.R
@@ -10,7 +10,7 @@
 #
 
 require(pacman)
-p_load(readxl, readr, janitor, dplyr)
+p_load(readr, readr, janitor, dplyr)
 require(here)
 
 #do we really need an output folder in the import task for this project?
@@ -22,18 +22,20 @@ getargs <- function() {
 		 input1 = here("import/input/flu_data_1_122219.txt"),
 		 output1 = here("import/output/flu1.txt"),
 		 input2 = here("import/input/flu_data_2_122219.txt"),
-		 output2 = here("import/output/flu2.txt"),)
+		 output2 = here("import/output/flu2.txt"))
 }
 
 main <- function() {
     args <- getargs()
-	read_excel(args$input1) %>%
+    read_tsv(args$input1) %>%
 		clean_names() %>%
-		write_delim(args$output, delim="|")
+		write_delim(args$output1, delim="|")
+    
+    read_tsv(args$input2) %>%
+      clean_names() %>%
+      write_delim(args$output2, delim="|")
 }
 
-#will the read_excel function change since these are txt files now?
-#how would I modify this code to import two files?
 
 main()
 
