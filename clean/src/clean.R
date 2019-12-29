735a6d3b6b85ddd6b28b08b126ad817d899ba0ec
unknown <jessica.morgan.randall@emory.edu>
2019-12-29 17:00:45 -0500

    updated src code for clean task

--- clean/src/clean.R
+++ clean/src/clean.R
@@ -9,76 +9,31 @@
 # HRDAGblog_JR/clean/src/clean.R
 #
 require(pacman)
-p_load(dplyr,styler,tidyverse,forcats,readr,janitor)
-require(here)
+p_load(dplyr,styler,tidyverse,forcats,readr,janitor,assertr)
+require(here, assertr)
 
-
-files <- list(input=here::here("import/input/flu_data_1_122219.csv"),
-			  input2=here::here("import/input/flu_data_2_122219.rds"),
-              output=here::here("clean/output/flu1.csv"),
-			  output2=here::here("clean/output/flu2.rds"))
+files <- list(input1=here::here("clean/input/flu_data_1_122219.txt"),
+              input2=here::here("clean/input/flu_data_2_122219.txt"),
+              output1=here::here("clean/output/flu1_clean.rds"),
+              output2=here::here("clean/output/flu2_clean.rds"))
 stopifnot(is.list(files)== TRUE)
 
-#how could we perform the same tasks on both data sets if we did want to keep
-#them seperate? or would this be an argument in favor of merging them?
+#why did we want these in rds files?
+#want to add in variables which are counts of the number of times each date occurs called cases/day here or do it just before graphing?
 
-# exercises for JR:
-#   -- upgrade repeated type converstions to mutate_at
-#   -- recreate tests using assertr in the tidyr pipeline
-
-#do we want to keep in the specific questions around cases happening before and after
-#certain dates or to certain age groups or at certain times of day?
-
-brks <- (1:9)*10
-dt_boundary_01 <- as.Date("2019-08-01")
-dt_boundary_21 <- as.Date("2019-08-21")
-
-flu1 <- readr::read_delim(files$input, delim="|") %>%
-	mutate(date = as.Date(`date`)) %>%
-    mutate(DOD = as_factor(date),
-           TOD = as_factor(time),
-           status = as_factor(status),
-		   sex = as_factor(sex)) %>%
-    mutate(age = as.numeric(age)) %>%
-	mutate(age_cut = cut(age,
-                         breaks=brks,
-                         right = FALSE)) %>%
-    mutate(age_grp = as_factor(age_cut)) %>%
-	mutate(dateb_20190801 = ifelse(date < dt_boundary_01, "pre", "post"),
-		   dateb_20190821 = ifelse(date < dt_boundary_21, "pre", "post")) %>%
-    select(-age_cut) %>%
-	assert(ncol(.) == 14) %>%
-	# i expect to have 3 rows with age_grp missing 
-	#bc 3 people have age data missing
-	verify(num_rows_na(age_grp)== 3) %>%
-	verify(not_na(DOD, TOD, living_conditions,
-				  dateb_20190821, dateb20190801)) %>%
-    write_delim(files$output, delim="|")
-stopifnot(nrow(flu1) == 109) #break it first, 107 breaks it
+flu1 <- readr::read_delim(files$input1, delim="|") %>%
+  mutate(age = as.integer(Age)) %>%
+  mutate(DOD = as.Date(`Date`, "%m/%d/%Y")) %>%
+  mutate_at(vars(DOD,Sex, Status), as.factor) %>%
+write_delim(files$output1, delim="|", na= "NA")
+stopifnot(ncol(flu1) == 7 & (nrow (flu1) == 108))
 
 flu2 <- readr::read_delim(files$input2, delim="|") %>%
-	mutate(date = as.Date(`date`)) %>%
-    mutate(DOD = as_factor(date),
-           TOD = as_factor(time),
-           status = as_factor(status),
-		   sex = as_factor(sex)) %>%
-    mutate(age = as.numeric(age)) %>%
-	mutate(age_cut = cut(age,
-                         breaks=brks,
-                         right = FALSE)) %>%
-    mutate(age_grp = as_factor(age_cut)) %>%
-	mutate(dateb_20190801 = ifelse(date < dt_boundary_01, "pre", "post"),
-		   dateb_20190821 = ifelse(date < dt_boundary_21, "pre", "post")) %>%
-    select(-age_cut) %>%
-	assert(ncol(.) == 14) %>%
-	# i expect to have 3 rows with age_grp missing 
-	#bc 3 people have age data missing
-	verify(num_rows_na(age_grp)== 3) %>%
-	verify(not_na(DOD, TOD, living_conditions,
-				  dateb_20190821, dateb20190801)) %>%
-    write_delim(files$output2, delim="|")
-	
-flu2 <- readr::read_delim(files$cleanflu2, delim=",")
-stopifnot(nrow(flu1) == 975) #break it first, 900 breaks it
+  mutate_at(vars(Sex, Status), as.factor) %>%
+  mutate(age = as.integer(Age)) %>%
+  mutate(DOD = as.Date(`Date`, "%m/%d/%Y")) %>%
+write_delim(files$output2, delim="|", na= "NA")
+stopifnot(ncol(flu2) == 7 & (nrow (flu2) == 974))
+
 
 ######done####
