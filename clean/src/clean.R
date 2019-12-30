20cc5922018b71e53b6e07c4a606aefc66d95200
unknown <jessica.morgan.randall@emory.edu>
2019-12-30 12:43:08 -0500

    updated flu to deaths

--- clean/src/clean.R
+++ clean/src/clean.R
@@ -10,32 +10,32 @@
 #
 require(pacman)
 p_load(dplyr,styler,tidyverse,forcats,readr,janitor,assertr)
-require(here, assertr)
 
-files <- list(input1=here::here("clean/input/flu_data_1_122219.txt"),
-              input2=here::here("clean/input/flu_data_2_122219.txt"),
-              output1=here::here("clean/output/flu1_clean.txt"),
-              output2=here::here("clean/output/flu2_clean.txt"))
+files <- list(input1=here::here("clean/input/death_data_1_122219.txt"),
+              input2=here::here("clean/input/death_data_2_122219.txt"),
+              output1=here::here("clean/output/death1_clean.txt"),
+              output2=here::here("clean/output/death2_clean.txt"))
 stopifnot(is.list(files)== TRUE)
 
 #why did we want these in rds files?
 #want to add in variables which are counts of the number of times each date occurs called cases/day here or do it just before graphing?
 
-flu1 <- readr::read_delim(files$input1, delim="|") %>%
+death1 <- readr::read_delim(files$input1, delim="|") %>%
   clean_names() %>%
   mutate(age = as.integer(age)) %>%
   mutate(DOD = as.Date(`date`, "%m/%d/%Y")) %>%
   mutate_at(vars(DOD,sex,status), as.factor) %>%
 write_delim(files$output1, delim="|", na= "NA")
-stopifnot(ncol(flu1) == 6 & (nrow (flu1) == 108))
+stopifnot(ncol(death1) == 6 & (nrow (death1) == 108))
 
-flu2 <- readr::read_delim(files$input2, delim="|") %>%
+death2 <- readr::read_delim(files$input2, delim="|") %>%
   clean_names() %>%
   mutate(age = as.integer(age)) %>%
   mutate(DOD = as.Date(`date`, "%m/%d/%Y")) %>%
   mutate_at(vars(DOD,sex,status), as.factor) %>%
 write_delim(files$output2, delim="|", na= "NA")
-stopifnot(ncol(flu2) == 6 & (nrow (flu2) == 974))
+stopifnot(ncol(death2) == 6 & (nrow (death2) == 974))
 
 
 ######done####
+
