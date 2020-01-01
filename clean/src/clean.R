c7f14d09cf3dc15e0de4bd0871907a67f1c9e547
unknown <jessica.morgan.randall@emory.edu>
2019-12-31 19:19:03 -0500

    removed sex and age vars

--- clean/src/clean.R
+++ clean/src/clean.R
@@ -25,35 +25,27 @@ dt_boundary_21 <- as.Date("2019-08-21")
 
 death1 <- readr::read_delim(files$input1, delim="|") %>%
   clean_names() %>%
-  mutate(age = as.integer(age)) %>%
   mutate(DOD = as.Date(`date`, "%Y/%m/%d")) %>%
   mutate(dateb_20190821 = ifelse(date < dt_boundary_21, "pre", "post"))  %>%
-  mutate_at(vars(sex,status,dateb_20190821), as.factor) 
+  mutate_at(vars(status,dateb_20190821), as.factor) 
 
 # retain only people with status "dead" and add in dates with no deaths
 death1 <-
   filter(death1, status == "dead") %>%
   write_delim(files$output1, delim="|")
-stopifnot(ncol(death1) == 7 & (nrow (death1) == 105)) 
+stopifnot(ncol(death1) == 4 & (nrow (death1) == 105)) 
 
 death2 <- readr::read_delim(files$input2, delim="|") %>%
   clean_names() %>%
-  mutate(age = as.integer(age)) %>%
   mutate(DOD = as.Date(`date`, "%Y/%m/%d")) %>%
   mutate(dateb_20190821 = ifelse(date < dt_boundary_21, "pre", "post"))  %>%
-  mutate_at(vars(sex,status, dateb_20190821), as.factor)
+  mutate_at(vars(status, dateb_20190821), as.factor)
 
 #retain only people with status "dead" and add in dates with no deaths
 death2 <-
   filter(death2, status == "dead") %>%
 write_delim(files$output2, delim="|")
-stopifnot(ncol(death2) == 7 & (nrow (death2) == 678))
-
-#specify getmode function
-getmode <- function(v) {
-  uniqv <- unique(v)
-  uniqv[which.max(tabulate(match(v, uniqv)))]
-}
+stopifnot(ncol(death2) == 4 & (nrow (death2) == 678))
 
 ######done####
 
